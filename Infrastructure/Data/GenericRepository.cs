375c674e3062aa1e933b4d57f78a7ee99f3f2f49
milicapavlovic96 <pavlovic380@gmail.com>
2022-03-20 14:17:07 +0100

    everything about orders in api done, also for indentity on frontend

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,10 +11,25 @@ namespace Infrastructure.Data
     public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
     {
         private readonly StoreContext _context;
-        public GenericRepository(StoreContext context){
+        public GenericRepository(StoreContext context)
+        {
             _context = context;
         }
 
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
 
         public async Task<T> GetByIdAsync(int id)
         {
@@ -34,13 +48,19 @@ namespace Infrastructure.Data
 
         public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
         {
-            return await ApplySpecification(spec).ToListAsync();
+            var query = ApplySpecification(spec);
+
+            return await query.ToListAsync();
         }
-        public async Task<int> CountAsync(ISpecification<T> spec)
+
+        public void Update(T entity)
         {
-            return await ApplySpecification(spec).CountAsync();
+            _context.Set<T>().Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
         }
-        private IQueryable<T> ApplySpecification(ISpecification<T> spec){
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+        {
             return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }
     }
--- client/src/app/core/interceptors/loading.interceptors.ts
+++ client/src/app/core/interceptors/loading.interceptors.ts
@@ -10,6 +10,7 @@ export class LoadingInterceptor implements HttpInterceptor {
     constructor(private busyService: BusyService) {
     }
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        
         this.busyService.busy();
         return next.handle(req).pipe(
             delay(1000),
